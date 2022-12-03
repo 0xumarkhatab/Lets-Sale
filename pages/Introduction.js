6c41a506151f6e5b6a91aa7044adeb2bb01d8c16
Umar Khatab <umarkhatab467@gmail.com>
2022-12-03 18:40:34 +0500

    Update Introduction.js

--- pages/Introduction.js
+++ pages/Introduction.js
@@ -23,15 +23,6 @@ export default function Introduction(props) {
         </div>
       </div>
       <div className={styles.container2}>
-//         <img
-//           className={styles.image}
-//           src={
-//             image
-//               ? image
-//               : "https://cdnb.artstation.com/p/assets/images/images/051/104/991/large/nft-artist-whatsapp-image-2022-06-28-at-6-14-42-pm-1.jpg?1656465338"
-//           }
-//           alt="bordape"
-//         />
        <img
           className={styles.image}
           src={
